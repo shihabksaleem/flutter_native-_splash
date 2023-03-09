b3b217b25416cacd88be0614ca2e95c78961ea56
shihabksaleem <shihab.ks56@gmail.com>
2023-03-09 10:28:01 +0530

    dependency updated

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_native_splash_sample/constants.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class ScreenLogin extends StatefulWidget {
